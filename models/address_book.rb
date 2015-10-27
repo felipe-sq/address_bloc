c05ffcc2e2f9e8c7bd9eaf7ef879e6f74591a1fd
Felipe Quintero <ffquint@gmail.com>
2015-10-27 12:03:58 -0700

    Updated address_book.rb: changed instance variable to local variable

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,14 +20,12 @@ class AddressBook
   end
 # Code for assignment: remove_entry method.
   def remove_entry(name, phone_number, email)
-    delete_entryj = nil
 
     @entries.each do |entry|
       if name == entry.name && phone_number == entry.phone_number && email == entry.email
-        @delete_entry = entry
+        delete_entry = entry
       end
+      @entries.delete(delete_entry)
     end
-
-    @entries.delete(@delete_entry)
   end
 end
